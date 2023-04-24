de0f98659b02266ce7467b8a31a87378eb5a9abe
AnnaGolitsyna <110576gav@gmail.com>
2023-04-24 08:47:11 +0300

    bg

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles
+    {/* <GlobalStyles
       styles={{
         body: {
           backgroundColor: '#e7efec',
         },
       }}
-    />
+    /> */}
     <App />
   </React.StrictMode>
 );
