0f434dc5b71e3cc7c85a0f6b726313b769496e04
AnnaGolitsyna <110576gav@gmail.com>
2023-06-21 16:50:39 +0300

    testInitial

---
+++ src/flights/components/searchFlights/SearchFlights.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Import the necessary dependencies
+import SearchFlights from './SearchFlights';
+
+const theme = createTheme();
+
+test('renders "FLIGHT SEARCH" text', () => {
+  render(
+    <Router>
+      <ThemeProvider theme={theme}>
+        <SearchFlights />
+      </ThemeProvider>
+    </Router>
+  );
+  const linkElement = screen.getByText(/FLIGHT SEARCH/i);
+  expect(linkElement).toBeInTheDocument();
+});
