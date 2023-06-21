56d2311dca207092470e72f20841a6744ed2a324
AnnaGolitsyna <110576gav@gmail.com>
2023-06-21 17:12:15 +0300

    testForText

--- src/flights/components/notFlights/TextAfterWar.test.jsx
+++ src/flights/components/notFlights/TextAfterWar.test.jsx
@@ -1,18 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles'; 
-import SearchFlights from './SearchFlights';
+import TextAfterWar from './TextAfterWar';
 
 const theme = createTheme();
 
-test('renders "FLIGHT SEARCH" text', () => {
+test('renders "No flights" text', () => {
   render(
     <Router>
       <ThemeProvider theme={theme}>
-        <SearchFlights />
+        <TextAfterWar />
       </ThemeProvider>
     </Router>
   );
-  const linkElement = screen.getByText(/FLIGHT SEARCH/i);
+  const linkElement = screen.getByText(/Please note that air traffic has been suspended since 24.02.22 due to the war/i);
   expect(linkElement).toBeInTheDocument();
 });
