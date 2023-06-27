fee289466c86926930af52e9ad5940992a35b47f
AnnaGolitsyna <110576gav@gmail.com>
2023-06-27 14:11:43 +0300

    navList

--- src/flights/components/navBar/NavBar.jsx
+++ src/flights/components/navBar/NavBar.jsx
@@ -1,17 +1,25 @@
 import StyledBox from './styledComponnent';
 import { NavLink } from 'react-router-dom';
 
+const navList = [
+  'For passanger',
+  'IEV Services',
+  'VIP',
+  'Corporate',
+  'Press center',
+  'UA',
+];
+
 const preventDefault = (event) => event.preventDefault();
 
 const NavBar = () => {
   return (
     <StyledBox sx={{ typography: 'body1' }} onClick={preventDefault}>
-      <NavLink to="/later">For passanger</NavLink>
-      <NavLink to="/later1">IEV Services</NavLink>
-      <NavLink to="/later2">VIP</NavLink>
-      <NavLink to="/later3">Corporate</NavLink>
-      <NavLink to="/later4">Press center</NavLink>
-      <NavLink to="/later5">UA</NavLink>
+      {navList.map((navName, ind) => (
+        <NavLink key={navName} to={`/Later${ind}`}>
+          {navName}
+        </NavLink>
+      ))}
     </StyledBox>
   );
 };
